e33605758140f3c87ab31faa1d618f9008f83e3b
merpinped <92573883+merpinped@users.noreply.github.com>
2023-12-01 21:06:56 +0100

    Ejercicio 3 Actualizado

--- ejercicio_1_3/mapper.py
+++ ejercicio_1_3/mapper.py
@@ -12,4 +12,4 @@ for line in sys.stdin:
         data = line.strip().split("\t")
 	if (len(data) != 6): continue
         date, time, store, item, cost, payment = data
-        print(method+"\t"+cost)
+        print(payment+"\t"+cost)
