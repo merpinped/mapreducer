464e5b0c06d3af43e9330eb7ffa9184ff0766796
merpinped <tics.dima.mera@gmail.com>
2023-12-02 01:34:41 +0100

    Ejercicio 4 creado

---
+++ ejercicio_1_4/reducer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import sys
+
+oldKey = None
+
+ventasMasAltas = {}
+
+# Loop around the data
+# It will be in the format key\tval
+# Where key is the store name, val is the sale amount
+#
+# All the sales for a particular store will be presented,
+# then the key will change and we'll be dealing with the next store
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 2:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    thisKey, thisSale = data_mapped
+
+    # Escribe un par key:value no dicc ante un cambio na key
+    if thisKey in ventasMasAltas:
+	if(ventasMasAltas[thisKey] < float(thisSale)): ventasMasAltas[thisKey] = float(thisSale)
+    else:
+        ventasMasAltas[thisKey] = float(thisSale)
+
+    # Comprueba si la venta guardada en el diccionario es menor que la nueva venta en esta linea.
+    oldKey = thisKey
+    
+
+# Escribe o diccionario cas ventas mais altas para cada metodo de pago
+if oldKey != None:
+    for m, v in ventasMasAltas.items():
+	print(m+"\t"+str(v))
